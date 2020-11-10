3dd8bcd3ba75e3a798b875f35a8e4e579e802caf
joshw507 <joshua.wallace@unilever.com>
2020-11-10 14:06:59 +0000

    testing config

--- percy-test/cypress/integration/examples/home.spec.js
+++ percy-test/cypress/integration/examples/home.spec.js
@@ -1,7 +1,7 @@
 describe("Integration test with visual testing", function() {
   it("Loads the homepage", function() {
     // Load the page or perform any other interactions with the app.
-    cy.visit("http://localhost:8081/");
+    cy.visit("http://localhost:8080/");
 
     cy.screenshot();
     cy.percySnapshot();
