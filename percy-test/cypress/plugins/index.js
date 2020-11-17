ab9965a64073cf84a75fa6152d4220b4f201f529
joshw507 <joshua.wallace@unilever.com>
2020-11-17 13:46:11 +0000

    test

--- percy-test/cypress/plugins/index.js
+++ percy-test/cypress/plugins/index.js
@@ -15,10 +15,6 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-};
 
 // In cypress/plugins/index.js
 let percyHealthCheck = require("@percy/cypress/task");
